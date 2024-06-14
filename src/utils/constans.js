d660ee3c911286dddd46df4be2b0bb13cb9acfc2
Luyen Dinh <luyen1368@gmail.com>
2024-06-14 22:51:59 +0700

    update BUILD MODE

--- src/utils/constans.js
+++ src/utils/constans.js
@@ -1,1 +1,13 @@
-export const API_ROOT = 'http://localhost:8017'
+// export const API_ROOT = 'http://localhost:8017'
+// export const API_ROOT = 'https://trello-api-b6f7.onrender.com'
+
+let apiRoot = ''
+if(process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+
+if(process.env.BUILD_MODE === 'production'){
+  apiRoot = 'https://trello-api-b6f7.onrender.com'
+}
+
+export const API_ROOT = apiRoot
